2c2f52fbe064108017c6dd3b02ba6eb4ce9f7ed0
James Lee <james@vcnc.co.kr>
2014-04-13 18:09:19 +0900

    handle http character entity

--- rss_url_filter.rb
+++ rss_url_filter.rb
@@ -2,7 +2,11 @@ module Jekyll
   module RSSURLFilter
     def relative_urls_to_absolute(input)
       url = "http://example.com"
-      input.gsub('src="/', 'src="' + url + '/').gsub('href="/', 'href="' + url + '/')
+      input\
+        .gsub('src="/', 'src="' + url + '/')\
+        .gsub('href="/', 'href="' + url + '/')\
+        .gsub('src=&quot;/', 'src=&quot;' + url + '/')\
+        .gsub('href=&quot;/', 'href=&quot;' + url + '/')
     end
   end
 end
